2d87a04bb79d8548a5f9010f7a2ba53e083bd9da
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-09-24 00:51:10 -0400

    test2

---
+++ 2_file_upload.js
@@ -0,0 +1,3 @@
+import lighthouse from "@lighthouse-web3/sdk";
+
+const uploadResponse = await lighthouse.upload('father.png', '92f721db.f897d995f402440d81017a6c000fedb5');
--- file_upload.js
+++ file_upload.js
@@ -1,16 +1,55 @@
-const lighthouse = require('@lighthouse-web3/sdk');
+// Lighthouse SDKのインポート{
 
-const filePath = './farther.ping';
+const filePath = './farther.png';
+const apiKey = '2f721db.f897d995f402440d81017a6c000fedb5';
 
-const apikey = '92f721db.f897d995f402440d81017a6c000fedb5';
-
+// アップロード用の関数
 async function uploadFile(filePath, apiKey) {
 
-  const uploadResponse = await lighthouse.upload(filePath, apiKey);
+  console.log('Starting upload...');
 
-  const cid = uploadResponse.cid;
+  try {
+    const uploadResponse = await lighthouse.upload(filePath, apiKey);  
+    const cid = uploadResponse.cid;
 
-  console.log(`Uploaded file, CID: ${cid}`);
+    console.log('Upload success!', cid);  
+    return cid;
 
-  return cid;
+  } catch (err) {
+    console.error('Upload failed', err);
+    throw err;
+  }
 }
+
+// ダウンロード用の関数  
+async function downloadFile(cid, filePath) {
+
+  console.log('Starting download...');
+
+  try {
+    // ダウンロード処理
+
+    console.log('Download success!');
+
+  } catch (err) {
+    console.error('Download failed', err);
+    throw err;  
+  }
+
+}
+
+// メイン処理
+async function main() {
+
+  // 各種パラメータ
+
+  const lighthouse = await import('@lighthouse-web3/sdk');
+
+  const cid = await uploadFile(filePath, apiKey); 
+  await downloadFile(cid, downloadPath);
+
+  console.log('Process completed!');
+
+}
+
+main();
