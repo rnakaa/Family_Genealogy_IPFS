04a72b810cf155e61a3b24226e51ee2366d618bf
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-09-24 01:13:50 -0400

    file upload

--- 2_file_upload.js
+++ 2_file_upload.js
@@ -1,3 +1,7 @@
 import lighthouse from "@lighthouse-web3/sdk";
 
+const dealParams = {
+	num_copies: 2,
+};
+
 const uploadResponse = await lighthouse.upload('father.png', '92f721db.f897d995f402440d81017a6c000fedb5');
